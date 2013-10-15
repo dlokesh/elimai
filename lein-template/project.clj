887225b9f3fdf3228d15e5cb389e0e337461239e
Lokeshwaran <dlokesh@gmail.com>
2013-10-15 06:39:37 +0530

    bumped up lein-template to v0.1.3

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,6 +1,6 @@
-(defproject elimai/lein-template "0.1.2"
+(defproject elimai/lein-template "0.1.3"
   :description "Leiningen template for elimai"
   :url "http://github.com/dlokesh/elimai/lein-template"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}            
   :eval-in-leiningen true)
